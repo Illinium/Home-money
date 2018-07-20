b690a6479d59b61aafbd8ea3086b21c1dad3bb62
Illinium <khodorko88@gmail.com>
2018-07-21 02:27:09 +0300

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import {UsersService} from './shared/services/users.service';
 
 
 
@@ -18,7 +19,7 @@ import {AppRoutingModule} from './app-routing.module';
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
